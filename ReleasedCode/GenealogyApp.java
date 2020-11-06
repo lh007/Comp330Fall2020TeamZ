f256839c6b376bb6267c71c0f7f69c01a05524b4
kaylin-luck <kluck@luc.edu>
2020-11-06 14:38:52 -0600

    refactored main

--- ReleasedCode/GenealogyApp.java
+++ ReleasedCode/GenealogyApp.java
@@ -14,6 +14,7 @@ public class GenealogyApp {
         AddPersonGUI ap = new AddPersonGUI();
         int currentID = 32;
         Person p = null;
+        boolean done = false;
 
         try {
             gdb.plantTree();
@@ -21,24 +22,49 @@ public class GenealogyApp {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.println("Would you like to add a person?");
+        do {
+        System.out.println("What would you like to do? Type a number" + "\n" + "1. Add a person" + "\n" + "2. Search in tree" + "\n" + "3. Exit app");
         String response = keyboard.next();
-        //statements for "Edit", "Search", "Add", "Exit"
-        if (response.equals("yes")) {
+        //statements for "Search", "Add", "Exit"; "edit" to come in cycle 3
+
+            if (response.equals("1")) {
                 ap.GUI(gdb.getMales(), gdb.getFemales(), gdb.getAllPeople(), currentID);
                 p = ap.newPerson;
                 gdb.geneMap.put(p.getID(), p);
                 currentID++;
-        }
-            System.out.println("Person successfully added to database!");
-            System.out.println("Are you done?");
-            String exit = keyboard.next();
-            if (exit.equals("yes")) {
+                System.out.println("Person successfully added to database!");
+            } else if (response.equals("2")) {
+                //for cycle 3, will be sent to SearchGUI instead of console
+                System.out.println("What are you looking for?" + "\n" + "1. All data about one person" + "\n" + "2. Siblings of a person" + "\n" + "3. Parents of a person" + "\n" + "4. Grandparents of a person");
+                String option = keyboard.next();
+                System.out.println("Enter person ID number, formatted as 'P#'");
+                String idnum = keyboard.next();
+                if (option.equals("1")) {
+                    System.out.println(gdb.geneMap.get(idnum).rawData());
+                } else if (option.equals("2")) {
+                    Object[] sibs = gdb.getSiblings(idnum).toArray();
+                    System.out.println("This persons siblings are: ");
+                    for (Object o : sibs) {
+                        System.out.println(o.toString());
+                    }
+                } else if (option.equals("3")) {
+                    System.out.println("This persons parents are: ");
+                    System.out.println(gdb.geneMap.get(idnum).getParents().toString());
+                } else if (option.equals("4")) {
+                    System.out.println("This persons grandparents are: ");
+                    System.out.println(gdb.getGrandparents(idnum).toString());
+                }
+            } else if (response.equals("3")) {
                 try {
                     op.writeResults();
+                    done = true;
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
+            } else {
+                System.out.println("Invalid selection, please retype");
             }
-        }
+        }while(!done);
     }
+
+}
