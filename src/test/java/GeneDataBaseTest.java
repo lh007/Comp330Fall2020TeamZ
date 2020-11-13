1397ab206439b3f6ddd3e6364e7f82fbd54f6de6
kendall <kendallnicole011@gmail.com>
2020-11-12 20:53:01 -0600

    fixed bug for P26 and P27

---
+++ src/test/java/GeneDataBaseTest.java
@@ -0,0 +1,58 @@
+package test.java;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import main.java.GeneDataBase;
+
+public class GeneDataBaseTest {
+    
+    @Test
+    public void testCreatePartnership1() {
+
+    }
+
+    @Test
+    public void testCreateParents1() {
+
+    }
+
+    @Test
+    public void testFindPerson1() {
+
+    }
+
+    @Test
+    public void testFindExactNameID1() {
+
+    }
+
+    @Test
+    public void testFindExactSurnameID1() {
+
+    }
+
+    @Test
+    public void testFindExactFirstNameID1() {
+
+    }
+
+    @Test
+    public void testGetChildren1() {
+
+    }
+
+    @Test
+    public void testGetParents() {
+
+    }
+
+    @Test
+    public void testGetGrandparents1() {
+
+    }
+
+    @Test
+    public void testGetSiblings1() {
+
+    }
+}
--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -9,7 +9,7 @@ public class PersonTest {
     // for living person
     @Test
     public void testDateAge1() {
-        Person sut = new Person(new String[]{" "," "," "," ","11/18/1998"," "," "," "," "});
+        Person sut = new Person(new String[]{" "," "," "," ","11/18/1998"," "," "," "," "," "});
         assertEquals("11/18/1998", sut.getDob());
         assertEquals(21, sut.getAge());
         assertEquals("N/a", sut.getDod());
@@ -18,7 +18,7 @@ public class PersonTest {
     // for dead person w/o dob
     @Test
     public void testDateAge2() {
-        Person sut = new Person(new String[]{" "," "," "," "," "," ","1/20/2020"," "," "});
+        Person sut = new Person(new String[]{" "," "," "," "," "," ","1/20/2020"," "," "," "});
         assertEquals("N/a", sut.getDob());
         assertEquals(0, sut.getAge()); // age == 0 because they do not have a birth date
         assertEquals("1/20/2020", sut.getDod());
@@ -27,7 +27,7 @@ public class PersonTest {
     // for dead person w/ dob
     @Test
     public void testDateAge3() {
-        Person sut = new Person(new String[]{" "," "," "," ","1/30/1900"," ","1/20/2020"," "," "});
+        Person sut = new Person(new String[]{" "," "," "," ","1/30/1900"," ","1/20/2020"," "," "," "});
         assertEquals("1/30/1900", sut.getDob());
         assertEquals(119, sut.getAge()); // age keeps counting up even if they have died
         assertEquals("1/20/2020", sut.getDod());
@@ -35,7 +35,7 @@ public class PersonTest {
 
     @Test
     public void testName1() {
-        Person sut = new Person(new String[]{" ","Johnson","Dick","Jr"," "," "," "," "," "});
+        Person sut = new Person(new String[]{" ","Johnson","Dick","Jr"," "," "," "," "," "," "});
         assertEquals("Johnson", sut.getFamilyName());
         assertEquals("Dick", sut.getGivenName());
         assertEquals("Jr", sut.getSuffix());
@@ -43,13 +43,13 @@ public class PersonTest {
 
     @Test
     public void testID1() {
-        Person sut = new Person(new String[]{"P0"," "," "," "," ",""," "," "," "});
+        Person sut = new Person(new String[]{"P0"," "," "," "," ",""," "," "," "," "});
         assertEquals("P0", sut.getID());
     }
 
     @Test
     public void testBirthDeathPlace1() {
-        Person sut = new Person(new String[]{" "," "," "," "," ","Chicago"," ","Dallas"," "});
+        Person sut = new Person(new String[]{" "," "," "," "," ","Chicago"," ","Dallas"," "," "});
         assertEquals("Chicago", sut.getBirthPlace());
         assertEquals("Dallas", sut.getDeathPlace());
     }
