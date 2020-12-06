3430ef5870f00ec660b69e6d7d237e1aab849b81
kendall <kendallnicole011@gmail.com>
2020-12-05 22:40:57 -0600

    about to merge into my client

--- src/test/java/GeneDataBaseTest.java
+++ src/test/java/GeneDataBaseTest.java
@@ -2,11 +2,10 @@
 package test.java; //for VSCode fix
 
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.util.ArrayList;
 import java.util.HashMap;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
@@ -40,17 +39,29 @@ public class GeneDataBaseTest {
 
     @After
     public void tearDown() throws Exception { sut = null; assertNull(sut); }
+
+    /**
+     * tests that the correct Partnership data was successfully read in from 
+     * the file and added to the SUT (database)
+     */
+    @Test
+    public void testPlantedPartnerships() {
+        // TODO
+        fail("not implemented");
+        
+    }
     
     @Test
     public void testCreatePartnership1() {
         // TODO
-        fail('not implemented');
+        fail("not implemented");
+        sut.createPartnership(new String[]{" "," "," "," "," "," "," "," "});
     }
 
     @Test
     public void testCreateParents1() {
         // TODO
-        fail('not implemented');
+        fail("not implemented");
     }
 
     /* TODO test methods for adding new relationships */
@@ -58,78 +69,78 @@ public class GeneDataBaseTest {
     @Test
     public void testCreateNewMarriage1() {
         // TODO
-        fail('not implemented');
+        fail("not implemented");
     }
 
     @Test
     public void testGetMales1() {
         // TODO
-        fail('not implemented');
+        fail("not implemented");
     }
 
     @Test
     public void testGetFemales1() {
         // TODO
-        fail('not implemented');
+        fail("not implemented");
     }
 
     @Test
     public void testGetAllPeople1() {
         // TODO
-        fail('not implemented');
+        fail("not implemented");
     }
 
     @Test
     public void testFindPerson1() {
         // TODO
-        fail('not implemented');
+        fail("not implemented");
     }
 
     @Test
     public void testFindExactNameID1() {
         // TODO
-        fail('not implemented');
+        fail("not implemented");
     }
 
     @Test
     public void testFindExactSurnameID1() {
         // TODO
-        fail('not implemented');
+        fail("not implemented");
     }
 
     @Test
     public void testFindExactFirstNameID1() {
         // TODO
-        fail('not implemented');
+        fail("not implemented");
     }
 
     @Test
     public void testGetChildren1() {
         // TODO
-        fail('not implemented');
+        fail("not implemented");
     }
 
     @Test
     public void testGetParents1() {
         // TODO
-        fail('not implemented');
+        fail("not implemented");
     }
 
     @Test
     public void testGetGrandparents1() {
         // TODO
-        fail('not implemented');
+        fail("not implemented");
     }
 
     @Test
     public void testGetSiblings1() {
         // TODO
-        fail('not implemented');
+        fail("not implemented");
     }
 
     @Test
     public void testEditEntry1() {
         // TODO
-        fail('not implemented');
+        fail("not implemented");
     }
 }
